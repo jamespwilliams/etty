92770c3cb8859004a0c0e5635527cb8972a3a990
James Williams <jamesphwilliams@gmail.com>
2020-12-30 16:12:06 +0000

    Return Nodes from api endpoint

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -28,6 +29,39 @@ func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (s *Server) handleEtymology() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "hello world\n")
+		if r.Method != http.MethodGet {
+			httpError(w, http.StatusBadRequest)
+			return
+		}
+
+		word := r.URL.Query().Get("word")
+		if word == "" {
+			httpError(w, http.StatusBadRequest, "missing word parameter")
+			return
+		}
+
+		lang := r.URL.Query().Get("lang")
+		if lang == "" {
+			lang = "en"
+		}
+
+		node := s.ety.Lookup(etymology.Word{
+			Word:     word,
+			Language: lang,
+		})
+
+		if err := json.NewEncoder(w).Encode(node); err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
+}
+
+func httpError(w http.ResponseWriter, code int, message ...string) {
+	if len(message) > 0 {
+		http.Error(w, fmt.Sprintf("%s: %s", http.StatusText(code), message[0]), code)
+		return
+	}
+
+	http.Error(w, http.StatusText(code), code)
 }
